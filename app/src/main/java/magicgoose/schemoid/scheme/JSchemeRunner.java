759f41919ca509adce6eac38998b69857911fa2d
Anonymous <anonymous@example.com>
2015-09-10 23:01:37 +0300

    remove trash

--- app/src/main/java/magicgoose/schemoid/scheme/JSchemeRunner.java
+++ app/src/main/java/magicgoose/schemoid/scheme/JSchemeRunner.java
@@ -6,8 +6,6 @@ import android.content.res.Resources;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 

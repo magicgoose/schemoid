f342a44179d048d70c02716d70a99cdaecb2513f
Anonymous <anonymous@example.com>
2015-09-10 16:04:36 +0300

    some actions for buttons

--- app/src/main/java/magicgoose/schemoid/fragment/ReplFragment.java
+++ app/src/main/java/magicgoose/schemoid/fragment/ReplFragment.java
@@ -6,10 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import magicgoose.schemoid.R;
 
 public class ReplFragment extends Fragment {
+
+    private EditText codeEditText;
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,46 @@ public class ReplFragment extends Fragment {
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         return inflater.inflate(R.layout.repl, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        codeEditText = (EditText) view.findViewById(R.id.code_input);
+
+        view.findViewById(R.id.eb_eval).setOnClickListener(this::evalClick);
+
+        view.findViewById(R.id.eb_left_arrow).setOnClickListener(this::arrowClick);
+        view.findViewById(R.id.eb_right_arrow).setOnClickListener(this::arrowClick);
+
+        view.findViewById(R.id.eb_left_paren).setOnClickListener(this::parenClick);
+        view.findViewById(R.id.eb_right_paren).setOnClickListener(this::parenClick);
+    }
+
+    private void parenClick(final View view) {
+        final String paren = view.getId() == R.id.eb_left_paren ? "(" : ")";
+        codeEditText.getText().insert(codeEditText.getSelectionStart(), paren);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        this.codeEditText = null;
+    }
+
+    private void arrowClick(final View view) {
+        final int offset = view.getId() == R.id.eb_left_arrow ? -1 : 1;
+        moveCursor(offset);
+    }
+
+    private void moveCursor(final int shift) {
+        final int current = codeEditText.getSelectionStart();
+        final int next = current + shift;
+        final int nextClamped = Math.max(0, Math.min(codeEditText.length(), next));
+        codeEditText.setSelection(nextClamped);
+    }
+
+    private void evalClick(final View view) {
+        Toast.makeText(getActivity(), "Coming soon!", Toast.LENGTH_SHORT).show();
     }
 }
